22f25df2a00ba0df277834cb9c7d04f8da647e4f
albertopiffer <albertopiffer96@gmail.com>
2023-03-17 16:32:44 +0100

    START php-badwords

---
+++ index.php
@@ -0,0 +1,5 @@
+<form action="processa_testo.php" method="GET">
+    <input type="text" name="name" placeholder="paragrafo">
+    <input type="text" name="name" placeholder="bad word">
+    <button type="submit">Invia</button>
+</form>
